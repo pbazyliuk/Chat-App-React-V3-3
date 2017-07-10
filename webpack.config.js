679fe9e58d4ad4cfebd14b1a14d09ba82f343779
Pavlo Bazyliuk <pavlo.bazyliuk@eleks.com>
2017-07-10 15:55:41 +0300

    added eslint settings

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['react-hot-loader', 'babel-loader'] 
+        loaders: ['react-hot-loader', 'babel-loader', 'eslint-loader'] 
       },
       {
         test: /\.scss$/,
