27932267c3f3b9c95c5f765165f1f29fbd4d34c0
Pavlo Bazyliuk <pavlo.bazyliuk@eleks.com>
2017-07-10 13:30:34 +0300

    added router redux & webpack dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './index.js',
   
   output: {
-    path: 'build',
+    // path: 'build',
     //path: __dirname + "/build",
     filename: 'bundle.js'
   },
@@ -14,7 +14,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel-loader'] 
+        loaders: ['react-hot-loader', 'babel-loader'] 
       },
       {
         test: /\.scss$/,
