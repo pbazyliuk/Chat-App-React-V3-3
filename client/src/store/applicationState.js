87e49ec76493c38ccee43195f96b72685e08f31a
Pavlo Bazyliuk <pavlo.bazyliuk@eleks.com>
2017-07-14 11:06:53 +0300

    changed app structure, added node server part

---
+++ client/src/store/applicationState.js
@@ -0,0 +1,10 @@
+import INITIAL_UI_STATE from './uiState';
+import INITIAL_STORE_DATA from './storeData';
+
+
+const INITIAL_APPLICATION_STATE = {
+	uiState: INITIAL_UI_STATE,
+	storeData: INITIAL_STORE_DATA
+};
+
+export default INITIAL_APPLICATION_STATE;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+//Main starting point
+const express = require('express');
+// import express from 'express';
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+
+const app = express()
+const router = require('./router')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+//DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+//App Setup
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+
+//a comment
+
+//Server Setup
+const port = process.env.PORT || 8090
+const server = http.createServer(app);
+
+server.listen(port)
+console.log('server is listening on: ', port)
