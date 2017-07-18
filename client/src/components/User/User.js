4ff6bc78e17cce77ea324689d70e64f0635067ea
Pavlo Bazyliuk <pavlo.bazyliuk@eleks.com>
2017-07-18 19:59:12 +0300

    added avatar for online users

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -2,21 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
+
 import styles from './User.scss';
 
 class User extends React.Component {
 	render() {
-		const { _id, firstname, isLogged } = this.props;
+		const { _id, firstname, isLogged, authenticated } = this.props;
 
-		console.log(this.props);
+		console.log(isLogged);
 
 		return (
 			<Link to={`/chat/${_id}`} className={styles['chat-list__link']}>
 				<li className={styles['chat-list__item']}>
 					<div className={styles['avatar']}>
 						<img
-							className={styles['avatar__img']}
+							className={
+								isLogged === true
+									? `${styles['avatar__img']} ${styles['hide']}`
+									: `${styles['avatar__img']} ${styles['show']}`
+							}
 							src="../../../images/user-off-avatar.png"
+							alt="avatar-image"
+						/>
+
+						<img
+							className={
+								isLogged === true
+									? `${styles['avatar__img']} ${styles['show']}`
+									: `${styles['avatar__img']} ${styles['hide']}`
+							}
+							src="../../../images/user-on-avatar.png"
 							alt="avatar-image"
 						/>
 					</div>
@@ -56,5 +72,9 @@ class User extends React.Component {
 // ChatsItem.propTypes = {
 //   chat: PropTypes.object.isRequired
 // }
+
+// const mapStateToProps = state => {
+// 	return { authenticated: state.applicationState.uiState.authenticated };
+// };
 
 export default User;
